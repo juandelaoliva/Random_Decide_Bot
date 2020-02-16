9896a331c0bef7a0937bd9da9f632a7808f8ec16
juandelaoliva <juandelaoliva1@gmail.com>
2020-02-16 21:52:15 +0100

    config

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+const apiKey = process.env.apiKey;
+const authDomain = process.env.authDomain;
+const databaseURL = process.env.databaseURL;
+const projectId = process.env.projectId;
+const storageBucket = process.env.storageBucket;
+const messagingSenderId = process.env.messagingSenderId;
+const appId = process.env.appId;
+const measurementId = process.env.measurementId;
+
+module.exports = {
+    apiKey: apiKey,
+    authDomain: authDomain,
+    databaseURL: databaseURL,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId
+}
